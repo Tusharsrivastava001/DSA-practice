6d17d4f732f249f398376c9b2b2abf429e6c4504
Tushar srivastava <tusharsrivastava1725@gmail.com>
2025-07-06 22:43:15 +0530

    Create set.c

---
+++ map and set/set.c
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<set>
+#include<unordered_set>
+int main(){
+    std::unordered_set<int> s;
+    s.insert(990);
+    s.insert(45);
+    s.insert(78);
+    s.insert(78);
+    s.insert(425);
+    s.insert(781);
+    
+    std::cout<<s.size()<<" "<<std::endl;
+    for(int x : s){//using of the for each without index
+        std::cout<<x<<" "; 
+    }
+    int tar=78;
+    if(s.find(tar)!=s.end()){ 
+        
+    }
+    //exits or not 
+
+    
+    
+}
