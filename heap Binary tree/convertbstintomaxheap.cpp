03f2f68740c7e8c4bc3f00d7a5fc867547819c6c
Tushar srivastava <tusharsrivastava1725@gmail.com>
2025-08-01 11:25:23 +0530

    Create convertbstintomaxheap.cpp

---
+++ heap Binary tree/convertbstintomaxheap.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+#include<vector>
+class Node{
+    public:
+    int val;
+    Node*left;
+    Node*right;
+    Node(int val){
+        this->val=val;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+void inorder(Node*root,std::vector<int> &v){
+    if(root==NULL) return;
+     inorder(root->right,v);
+   
+    v.push_back(root->val);
+    inorder(root->left,v);
+    
+}
+void preorder(Node*root,std::vector<int> &v,int &i){
+    
+     if(root==NULL) return;
+     root->val=v[i++];
+     
+     preorder(root->left,v,i);
+     preorder(root->right,v,i);
+}
+int level(Node*root){
+    if(root==NULL) return 0;
+    return 1+level(root->left)+level(root->right);
+}
+void display(Node*root,int curr,int leveli){
+    if(root==NULL) return ;
+    if(curr==leveli){
+    std::cout<<root->val<<" ";
+    return;
+    }
+    display(root->left,curr+1,leveli);
+    display(root->right,curr+1,leveli);
+}
+void levelorder(Node*root){
+    int n=level(root);
+    for(int i=1;i<=n;i++){
+        display(root,1,i);
+        std::cout<<std::endl;
+    }
+}
+int main(){
+    Node*a=new Node(10);
+    Node*b=new Node(5);
+    Node*c=new Node(16);
+    Node*d=new Node(1);
+    Node*e=new Node(8);
+    Node*f=new Node(12);
+    Node*g=new Node(20);
+    a->left=b;
+    a->right=c;
+    b->left=d;
+    b->right=e;
+    c->left=f;
+    c->right=g;
+    std::vector<int> res;
+    inorder(a,res);
+    
+    for(int ele : res){
+        std::cout<<ele<<" ";
+    }
+    int i=0;
+    preorder(a,res,i);
+    std::cout<<std::endl;
+    
+    //here we aer copyint the revser sorte array into the bst 
+    //in the same tree
+    levelorder(a);
+    
+
+}
